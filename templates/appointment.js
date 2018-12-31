699c3850f961802a422b73785ecda7fb2505a2ef
richardison <richard.ison@carleton.ca>
2018-12-31 18:07:03 -0500

    add: appointment page

--- templates/appointment.js
+++ templates/appointment.js
@@ -1,5 +1,13 @@
 let appointmentPage = `
-    <section> 
-        appointment page
+    <section class="container relative"> 
+        <div id="page-banner"></div>
+        <div class="title-wrapper">
+            <h2 class="page-title">Make an Appointment</h2>
+        </div>
+        <p style="text-align: center; margin-top: -25px; font-size: 15px;"><em>Appointments are recommended. Please call to book.</em></p>
+        <p style="text-align: center; font-size: 28px; font-weight: bold; margin-top: 45px; margin-bottom: 35px;"><strong>(613) 695-4664</strong></p>
+        <p style="text-align: center;"><span style="font-size: 15px;">Limited dedicated parking. Street parking is also available.</span><br>
+            <span style="font-size: 15px;">If you need assistance finding parking, please contact us.</span>
+        </p>
     </section>
 `;
