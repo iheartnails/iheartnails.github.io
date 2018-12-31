47f24788e933bd4d8f6fa65e7655411c86d91f96
richardison <richard.ison@carleton.ca>
2018-12-31 18:07:12 -0500

    add: contact page

--- templates/contact.js
+++ templates/contact.js
@@ -1,5 +1,15 @@
 let contactPage = `
-    <section> 
-        contact page
+    <section class="container relative"> 
+        <div id="page-banner"></div>    
+        <div class="title-wrapper">
+            <h2 class="page-title"> Contact Us </h2>
+            <p class="page-subtitle"><em>We'd love to hear from you. </em></p>
+        </div>
+        <p style="text-align: center; font-size: 20px; margin-top: 45px; margin-bottom: 35px;">
+            <i class="fas fa-phone"></i>  <strong>(613) 695-4664</strong>
+        </p>
+        <p style="text-align: center; font-size: 20px; margin-top: 45px; margin-bottom: 35px;">
+            <i class="fas fa-map-marker-alt"></i>  1541 MERIVALE RD, OTTAWA, ON K2G5W1
+        </p>
     </section>
 `;
