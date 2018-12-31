2074ba4d6f3315d36ed9fb5734233d39b3fa8be8
richardison <richard.ison@carleton.ca>
2018-12-31 18:07:26 -0500

    add: homepage

--- templates/home.js
+++ templates/home.js
@@ -1,5 +1,31 @@
 let homePage = `
-    <section> 
-        home page
+    <section class="container relative"> 
+        <div id="home-banner">
+        <a href="#" onclick="onNavClick('/about')">
+            <img src="http://dnnailspa.ca/images/slideshow/slides/slide1.png">
+        </a>
+        </div>
+        <div class="clear"></div>
+        <div id="home-boxes"> 
+            <div class="home-box box1">
+                <a href="#" onclick="onNavClick('/services')">
+                    <h4 class="headline">Eyelashes</h4>
+                    <p class="excerpt"></p>
+                    <p class="link-text"><span class="button pie">View</span></p></a>
+            </div>
+            <div class="home-box box2">
+                <a href="#" onclick="onNavClick('/services')">
+                    <h4 class="headline">Hands and Feet</h4>
+                    <p class="excerpt"></p>
+                    <p class="link-text"><span class="button pie">View</span></p></a>
+            </div>
+            <div class="home-box box3">
+                <a href="#" onclick="onNavClick('/services')">
+                    <h4 class="headline">Waxing</h4>
+                    <p class="excerpt"></p>
+                    <p class="link-text"><span class="button pie">View</span></p></a>
+            </div>
+            <div class="clear"></div>
+        </div>
     </section>
 `;
